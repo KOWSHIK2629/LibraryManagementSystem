823c1c8f86ae3d53c911152868c1e36b18fa58ee
Batch1 <Batch1@VITUNIVERSITY.LOCAL>
2026-04-16 14:34:13 +0530

    library

--- src/main/java/com/library/LibraryService.java
+++ src/main/java/com/library/LibraryService.java
@@ -1,1 +1,24 @@
+package com.library;
 
+public class LibraryService {
+
+    public int calculateFine(int days) {
+        int allowedDays = 7;
+        int finePerDay = 10;
+
+        if (days > allowedDays) {
+            return (days - allowedDays) * finePerDay;
+        }
+        return 0;
+    }
+
+    public void display(User user) {
+        int fine = calculateFine(user.days);
+
+        System.out.println("User Name: " + user.name);
+        System.out.println("User ID: " + user.id);
+        System.out.println("Books Issued: " + user.booksIssued);
+        System.out.println("Days: " + user.days);
+        System.out.println("Fine: " + fine);
+    }
+}
